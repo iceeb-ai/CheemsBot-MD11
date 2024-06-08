fd627e011d6d2bbf7efa9d8de08070957f8b5108
Icee B <154991157+iceeb-ai@users.noreply.github.com>
2024-06-09 00:24:51 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,35 +15,35 @@ global.sessionid ='ur session id'
 
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "insta: iceeb2024" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'ɨƈɛɛ-𝕏 MD V11' //ur bot name
+global.ownernumber = '2349013204001' //ur owner number
+global.ownername = '‎𝗜cεε 𝗕' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "©️𝛪𝐶𝛯𝛯-𝕏"
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.packname = "‎𝗜cεε 𝗕"
+global.author = "‎𝗜cεε 𝗕\n\n+2349013204001"
+global.creator = "2349013204001@s.whatsapp.net"
+global.xprefix = '/'
+global.premium = ["2349013204001"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
+global.autoblocknumber = '9' //set autoblock country code
+global.antiforeignnumber = '9' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.groupevent = true //show update messages in group chat
 //msg
 global.mess = {
 	limit: 'Your limit is up!',
